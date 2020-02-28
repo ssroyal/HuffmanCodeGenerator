1b827c89689c21a1f362fb968a14658e88e156bd
saurabh <ssroyal>
2020-02-29 02:31:22 +0530

    update Main class add method to check only Alphabets

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,6 +11,7 @@ import binaryGenerator.BinaryGenerator;
 import huffmanGenerator.HuffmanGenerator;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
@@ -18,16 +19,33 @@ public class Main {
         final Scanner scanner = new Scanner(System.in);
         String userInput = scanner.nextLine().trim();
         userInput=userInput.toLowerCase();
-        BinaryGenerator binaryGenerator = new BinaryGenerator(userInput);
-        String binaryString = binaryGenerator.convertNormalStringToBinaryString();
-        HuffmanGenerator huffmanGenerator = new HuffmanGenerator(userInput);
-        String huffmanString = huffmanGenerator.convertNormalStringToHuffmanString();
-        int lengthOfBinaryString = binaryString.length();
-        int lengthOfHuffmanString = huffmanString.length();
-        System.out.println("The binary string has " + lengthOfBinaryString + " characters.");
-        System.out.println("The huffman string has " + lengthOfHuffmanString + " characters.");
-        System.out.printf("Compression Ratio (Huffman/Binary): %.2f %%",((double)lengthOfHuffmanString / (double) lengthOfBinaryString));
+        if(!onlyCharChecker(userInput))
+        {
+            System.out.printf("Sorry Only Alphabets are allowed in String");
+        }
+        else
+        {
+
+            BinaryGenerator binaryGenerator = new BinaryGenerator(userInput);
+            String binaryString = binaryGenerator.convertNormalStringToBinaryString();
+            HuffmanGenerator huffmanGenerator = new HuffmanGenerator(userInput);
+            String huffmanString = huffmanGenerator.convertNormalStringToHuffmanString();
+            int lengthOfBinaryString = binaryString.length();
+            int lengthOfHuffmanString = huffmanString.length();
+            System.out.println("The binary string has " + lengthOfBinaryString + " characters.");
+            System.out.println("The huffman string has " + lengthOfHuffmanString + " characters.");
+            System.out.printf("Compression Ratio (Huffman/Binary): %.2f %%",((double)lengthOfHuffmanString / (double) lengthOfBinaryString));
+        }
 
         scanner.close();
+    }
+    public static  boolean  onlyCharChecker(String mainString)
+    {
+        boolean response = false;
+        if(Pattern.matches("^[a-z' ']*$",mainString) )
+        {
+            response=true;
+        }
+        return response;
     }
 }
